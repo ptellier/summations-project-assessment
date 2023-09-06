6502e70751e5b77c37d36f14fedbad49561ca558
Phillip Tellier <ptellier@student.ubc.ca>
2023-09-06 16:38:31 -0700

    initialize next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Welcome to my Phillip's Summations Project Assessment!</h1>
+
+      </div>
+    </main>
+  )
+}
