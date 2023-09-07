71b6e2da7ca9e3aa7f498d6f5204e84c2fcf82c8
Phillip Tellier <ptellier@student.ubc.ca>
2023-09-07 06:39:58 -0700

    Fix: apostrophe ES Lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.header}>
-        <h1>Welcome to Phillip's Summations Assessment!</h1>
+        <h1>Welcome to Phillip&apos;s Summations Assessment!</h1>
         <h4>Phillip also just updated his website, check it out at <a className={styles.link} href="https://www.phillip.tel">www.phillip.tel</a></h4>
       </div>
       <DOIToAbstract />
